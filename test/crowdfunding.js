7e3be6242d3111ea870d52fa5a6eb2ae074a9db2
Ilie Andrei-Virgil <andreivirgil@hotmail.com>
2024-05-13 16:52:36 +0300

    ERC 20
    Added ERC 20

--- test/crowdfunding.js
+++ test/crowdfunding.js
@@ -1,25 +1,17 @@
-describe("Crowdfunding Contract", function () {
-    let ethers;
-
-    before(async function () {
-        try {
-            const hardhat = await import("hardhat");
-            console.log("Imported Hardhat:", hardhat);
-            ethers = hardhat.ethers;
-            console.log("Ethers loaded:", !!ethers);
-        } catch (error) {
-            console.error("Error importing Hardhat:", error);
-        }
-
-        if (!ethers) {
-            throw new Error("Ethers is not initialized");
-        }
+(async () => {
+    const chai = await import('chai');
+    const { expect } = chai;
+    const { ethers } = require('hardhat');
+  
+    describe("Basic Functionality Test", function () {
+      it("Should fetch the list of accounts", async function () {
+        const accounts = await ethers.getSigners();
+        console.log("Accounts:", accounts.map(account => account.address));
+        expect(accounts).to.be.an('array').that.is.not.empty;
+      });
     });
-
-    it("should test if ethers is loaded", function () {
-        console.log("Testing if ethers is loaded...");
-        if (!ethers) {
-            throw new Error("Failed to load ethers");
-        }
-    });
-});
+  
+    // This line ensures Mocha doesn't start before the imports are ready
+    run();
+  })();
+  
